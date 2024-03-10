176d48ef757dfe80e76898cc0b3faa5aa2e4edbc
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-03-10 13:46:36 -0400

    1ros Conceptos

--- jpaprueba/src/main/java/com/cristianml/jpaprueba/logica/Main.java
+++ jpaprueba/src/main/java/com/cristianml/jpaprueba/logica/Main.java
@@ -2,6 +2,20 @@ package com.cristianml.jpaprueba.logica;
 
 public class Main {
     public static void main(String[] args) {
+        // JPA = JAVA PERSISTENCE API, is an intermediary that allows us to 
+        // communicate our application with database by mapping through java annotations in parts of our project
+        // to convert classes into entities, variables into attributes, etc.
+        // We must to add the dependency mysql on pom.xml or add with right clic on Dependencies
+        // we connect database in Services -> clic derecho en databases
+        // -> new connection -> add mysql (si no lo tenemos) -> usuarios -> asus -> .m2 
+        // -> repositories -> mysql -> ..... -> ......
+        // Luego agregamos la base de datos dando clic derecho en databases seleccionando mysql
+        // agregamos los valores del servidor y escribimos el nombre de la bd
+        // luego creamos la unidad de persistencia en nuestro proyecto, dentro de la capa
+        // persistencia clic izquierdo -> new -> others -> persistence -> percistence unit
+        // -> cambiamos nombre implementando al final "PU", seleccionamos nuestra conexión
+        // creada anteriormente y seleccionamos "create".
+        
         
     }
 }
