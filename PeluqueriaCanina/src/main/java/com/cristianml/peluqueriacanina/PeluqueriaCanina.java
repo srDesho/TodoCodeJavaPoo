d108f87026186a37c0a507572a598ba4ceb4d02a
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-03-14 17:05:20 -0400

    Creando proyecto Peluquería canina

---
+++ PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cristianml.peluqueriacanina;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
