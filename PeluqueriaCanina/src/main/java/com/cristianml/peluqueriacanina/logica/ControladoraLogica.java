208ca34d48436bc9f93600bc8d60071c317c89a4
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-03-15 19:13:37 -0400

    Agregando métodos y relación entre la lógica y persistencia con método guardar

--- PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/logica/ControladoraLogica.java
+++ PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/logica/ControladoraLogica.java
@@ -1,5 +1,29 @@
 package com.cristianml.peluqueriacanina.logica;
 
-public class ControladoraLogica {
+import com.cristianml.peluqueriacanina.persistencia.ControladoraPersistencia;
 
+public class ControladoraLogica {
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+
+    public void guardar(String nombreMasco, String raza, String color, String alergico
+                , String atencioEsp, String obs, String nombreDuenio, String celDuenio){
+        // Creamos los objetos
+        // Dueño
+        Duenio duenio = new Duenio();
+        duenio.setNombre(nombreDuenio);
+        duenio.setCelular(celDuenio);
+        
+        // Masconta
+        Mascota masco = new Mascota();
+        masco.setNombre(nombreMasco);
+        masco.setRaza(raza);
+        masco.setColor(color);
+        masco.setAlergico(alergico);
+        masco.setAtencio_especial(atencioEsp);
+        masco.setObservaciones(obs);
+        masco.setUnDuenio(duenio);
+        
+        controlPersis.guardar(duenio, masco);
+    }
+    
 }
--- PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/persistencia/ControladoraPersistencia.java
+++ PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/persistencia/ControladoraPersistencia.java
@@ -1,6 +1,14 @@
 package com.cristianml.peluqueriacanina.persistencia;
 
+import com.cristianml.peluqueriacanina.logica.Duenio;
+import com.cristianml.peluqueriacanina.logica.Mascota;
+
 public class ControladoraPersistencia {
     MascotaJpaController mascoJpa = new MascotaJpaController();
     DuenioJpaController duenioJpa = new DuenioJpaController();
+
+    public void guardar(Duenio duenio, Mascota masco) {
+        duenioJpa.create(duenio);
+        mascoJpa.create(masco);
+    }
 }
