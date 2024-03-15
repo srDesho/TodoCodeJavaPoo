db53b23470923d46540c1b19fc4cd018543d6628
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-03-15 16:29:23 -0400

    Creando las clases con sus atributos y métodos

--- PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/logica/Duenio.java
+++ PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/logica/Duenio.java
@@ -3,5 +3,49 @@ package com.cristianml.peluqueriacanina.logica;
 
 
 public class Duenio {
+ private int id;
+    private String nombre;
+    private String celular;
+    private String direccion;
 
+    public Duenio() {
+    }
+
+    public Duenio(int id, String nombre, String celular, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.celular = celular;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
--- PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/logica/Mascota.java
+++ PeluqueriaCanina/src/main/java/com/cristianml/peluqueriacanina/logica/Mascota.java
@@ -1,7 +1,93 @@
-
 package com.cristianml.peluqueriacanina.logica;
 
-
 public class Mascota {
+    private int num_cliente;
+    private String nombre;
+    private String raza;
+    private String color;
+    private String alergico;
+    private String atencio_especial;
+    private String observaciones;
+    
+    private Duenio unDuenio;
 
+    public Mascota() {
+    }
+
+    public Mascota(int num_cliente, String nombre, String raza, String color, String alergico, String atencio_especial, String observaciones, Duenio unDuenio) {
+        this.num_cliente = num_cliente;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+        this.alergico = alergico;
+        this.atencio_especial = atencio_especial;
+        this.observaciones = observaciones;
+        this.unDuenio = unDuenio;
+    }
+
+    public int getNum_cliente() {
+        return num_cliente;
+    }
+
+    public void setNum_cliente(int num_cliente) {
+        this.num_cliente = num_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAlergico() {
+        return alergico;
+    }
+
+    public void setAlergico(String alergico) {
+        this.alergico = alergico;
+    }
+
+    public String getAtencio_especial() {
+        return atencio_especial;
+    }
+
+    public void setAtencio_especial(String atencio_especial) {
+        this.atencio_especial = atencio_especial;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public Duenio getUnDuenio() {
+        return unDuenio;
+    }
+
+    public void setUnDuenio(Duenio unDuenio) {
+        this.unDuenio = unDuenio;
+    }
+    
+    
 }
